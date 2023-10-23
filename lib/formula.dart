87768781df6601a7a5b37624276ad970ebeb6bd4
Niks_patel <nikunjpatel.tops@gmail.com>
2023-10-23 11:19:36 +0530

    Created new flutter project

---
+++ lib/formula.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+        appBar: AppBar(),
+
+        body: Center(
+
+        )
+      ),
+    );
+  }
+}
